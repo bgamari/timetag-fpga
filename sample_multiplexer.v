4b93a077dca51e0ec92d415a68a9a3a111749d4a
Ben Gamari <bgamari.foss@gmail.com>
2009-10-25 03:49:12 -0400

    Use actual continuous assignment in sample_multiplexer

--- sample_multiplexer.v
+++ sample_multiplexer.v
@@ -13,7 +13,7 @@ input data_ack;
 output [7:0] data;
 output data_rdy;
 
-reg [7:0] data;
+wire [7:0] data;
 
 reg [1:0] state;
 reg [2:0] byte_idx;
@@ -39,16 +39,14 @@ case (state)
 		state <= 2'b00;
 endcase
 
-always
-case (byte_idx)
-	3'd0: data = sample[47:40];
-	3'd1: data = sample[39:32];
-	3'd2: data = sample[31:24];
-	3'd3: data = sample[23:16];
-	3'd4: data = sample[15:8];
-	3'd5: data = sample[7:0];
-	default: data = 8'b0;
-endcase
+assign data[7:0] = 
+	(byte_idx == 3'd0) ? sample[47:40] :
+	(byte_idx == 3'd1) ? sample[39:32] :
+	(byte_idx == 3'd2) ? sample[31:24] :
+	(byte_idx == 3'd3) ? sample[23:16] :
+	(byte_idx == 3'd4) ? sample[15:8] :
+	(byte_idx == 3'd5) ? sample[7:0] :
+	8'b0;
 
 assign sample_ack = (state == 2'b10);
 assign data_rdy = (state == 2'b01);
