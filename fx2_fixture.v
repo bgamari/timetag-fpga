fa273b037cda48d5df914cc7b673a95a86d282cd
Ben Gamari <bgamari.foss@gmail.com>
2009-10-31 16:35:19 -0400

    Update FX2 fixture

--- fx2_fixture.v
+++ fx2_fixture.v
@@ -2,7 +2,7 @@
 
 // IN (device->host) endpoint FIFO
 module in_fifo(
-	ifclk, fifoadr, data, slwr, pktend
+	ifclk, fifoadr, data, slwr, pktend, full
 );
 parameter FIFOADR = 6;
 
@@ -12,45 +12,85 @@ input [1:0] fifoadr;
 input pktend;
 output [7:0] data;
 
-reg [7:0] buffer [1024:0];
-reg [8:0] wr_idx;
-
 always @(posedge ifclk)
 begin
 	if (fifoadr == FIFOADR && slwr)
-	begin
-		wr_idx = wr_idx + 1;
-		$display("%2b OUT %x", FIFOADR, buffer[wr_idx]);
-	end
+		$display("%2b IN %x", FIFOADR, buffer[tail]);
 	if (fifoadr == FIFOADR && pktend)
 		$display("%2b: PKTEND");
 end
-assign data = buffer[wr_idx];
+
+assign full = 0;
+assign data = buffer[tail];
 endmodule
 
 
 // OUT (host->device) endpoint FIFO:
 module out_fifo(
-	ifclk, fifoadr, data, slrd
+	ifclk, fifoadr, data, slrd, empty
+	data_in, data_in_wr, commit, send_done
 ); 
-parameter FIFOADR = 6;
+parameter FIFOADR = 2;
 
 input ifclk;
 input slrd;
 input [1:0] fifoadr;
 input [7:0] data;
+output empty;
+
+input [7:0] data_in;
+input data_in_wr;
+input commit;
+output send_done;
+
+reg [7:0] buffer [1024:0];
+reg [8:0] staged_length;
+reg [8:0] length; // Amount of data in buffer
+reg [8:0] tail; // Read-out index
+reg [8:0] head; // Read-in index
+reg send_done;
 
 always @(posedge ifclk)
 begin
-	if (fifoadr == FIFOADR && slrd)
-		$display("%2b: IN %x", fifoadr, data);
+	if (fifoadr == FIFOADR && slrd && length)
+	begin
+		head <= head + 1;
+		$display("%2b: OUT %x", fifoadr, data);
+	end
+
+	if (send_done)
+	begin
+		length <= 0;
+		head <= 0;
+		tail <= 0;
+	end
 end
+
+always @(posedge ifclk)
+begin
+	if (data_wr)
+	begin
+		buffer[tail] <= data;
+		staged_length <= staged_length + 1;
+		tail <= tail + 1;
+	end
+
+	if (commit)
+	begin
+		length = staged_length;
+		staged_length <= 0;
+	end
+end
+
+assign data = buffer[tail];
+assign send_done = (head == length) && (length != 0);
+assign empty = (length == 0);
 endmodule
 
 
 // FX2 model
 module fx2_test_fixture(
-	ifclk, fd, slrd, slwr, flags, sloe, fifoadr, pktend
+	ifclk, fd, slrd, slwr, sloe, fifoadr, pktend, flags
 );
 
 output ifclk;
@@ -80,7 +120,8 @@ out_fifo ep2(
 	.ifclk(ifclk),
 	.fifoadr(fifoadr),
 	.data(out_data),
-	.slrd(slrd)
+	.slrd(slrd),
+	.empty(flags[0])
 );
 defparam ep2.FIFOADR = 00;
 
@@ -88,7 +129,8 @@ in_fifo ep6(
 	.ifclk(ifclk),
 	.fifoadr(fifoadr),
 	.data(in_data[2]),
-	.slwr(slwr)
+	.slwr(slwr),
+	.full(flags[1])
 );
 defparam ep6.FIFOADR = 10;
 
