0fd744cc098b70831c16fae7f34c505967135b12
Ben Gamari <bgamari.foss@gmail.com>
2011-02-11 10:28:41 -0500

    Add comments to config.v

--- config.v
+++ config.v
@@ -1,2 +1,7 @@
+// Configuration options:
+
+// Increment for any major change
 `define HWVERSION 8'd2
+// Acquisition clockrate: Make sure you account for PLL parameters as well
 `define CLOCKRATE 8'd30
+
