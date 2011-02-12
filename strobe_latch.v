c02821b1b1167c548af619bb190dc9e69c45fa2f
Ben Gamari <bgamari.foss@gmail.com>
2011-02-12 14:33:02 -0500

    Revert "Greatly simplify strobe latch"
    This reverts commit f6b6f718dd1d3809d4eb4f9f23f6057285690a45.
    Unfortunately Quartus had trouble with two always blocks driving the
    same register

--- strobe_latch.v
+++ strobe_latch.v
@@ -5,14 +5,25 @@ input clk;
 input in;
 output out;
 
+reg state;
 reg out;
 initial out = 0;
 
-always @(posedge in)
-	out <= 1;
-
 always @(posedge clk)
-	if (out)
-		out <= 0;
+begin
+	if (in == 1 && state == 0)
+	begin
+		state <= 1;
+		out <= 1;
+	end
+	else
+	begin
+		if (out == 1)
+			out <= 0;
+	end
+	
+	if (in == 0)
+		state <= 0;
+end
 
 endmodule
