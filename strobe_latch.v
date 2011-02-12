f6b6f718dd1d3809d4eb4f9f23f6057285690a45
Ben Gamari <bgamari.foss@gmail.com>
2011-02-12 14:09:52 -0500

    Greatly simplify strobe latch
    As a side-effect, it should also now catch events where the input is
    brought high for less than one clock cycle

--- strobe_latch.v
+++ strobe_latch.v
@@ -5,25 +5,14 @@ input clk;
 input in;
 output out;
 
-reg state;
 reg out;
 initial out = 0;
 
+always @(posedge in)
+	out <= 1;
+
 always @(posedge clk)
-begin
-	if (in == 1 && state == 0)
-	begin
-		state <= 1;
-		out <= 1;
-	end
-	else
-	begin
-		if (out == 1)
-			out <= 0;
-	end
-	
-	if (in == 0)
-		state <= 0;
-end
+	if (out)
+		out <= 0;
 
 endmodule
