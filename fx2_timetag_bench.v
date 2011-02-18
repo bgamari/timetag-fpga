49c55c9c34b42bb9e8ac24d6bc57d11f9bd37cc2
Ben Gamari <bgamari.foss@gmail.com>
2011-02-17 22:40:59 -0500

    bench: Output records in a friendly way

--- fx2_timetag_bench.v
+++ fx2_timetag_bench.v
@@ -12,6 +12,7 @@ wire sloe, slrd, slwr, pktend;
 reg [3:0] delta_in;
 reg [3:0] strobe_in;
 
+wire [35:0] counter = uut.tagger.apdtimer.tagger.timer;
 
 // External Clock
 initial clk = 0;
@@ -20,22 +21,22 @@ always #2 clk = ~clk;
 // Simulate strobe inputs
 initial strobe_in = 0;
 always begin
-	#100 strobe_in[0] = 1;
+	#100 strobe_in[0] = 1; $display($time, "  Strobe channel 0 (counter=%d)", counter);
 	#5   strobe_in[0] = 0;
 end
 always begin
-	#80  strobe_in[1] = 1;
+	#80  strobe_in[1] = 1; $display($time, "  Strobe channel 1 (counter=%d)", counter);
 	#5   strobe_in[1] = 0;
 end
 
 // Simulate delta inputs
 initial delta_in = 0;
 always begin
-	#100 delta_in[0] = 1;
+	#100 delta_in[0] = 1; $display($time, "  Delta channel 0 (counter=%d)", counter);
 	#100 delta_in[0] = 0;
 end
 always begin
-	#200 delta_in[1] = 1;
+	#200 delta_in[1] = 1; $display($time, "  Delta channel 1 (counter=%d)", counter);
 	#300 delta_in[1] = 0;
 end
 
@@ -111,20 +112,30 @@ begin
 	@(cmd_sent);
 
 	@(posedge fx2_clk && reply_rdy); // HACK perhaps
-	@(posedge fx2_clk && reply_rdy);
-	reply_buf[7:0] = reply_data;
-	@(posedge fx2_clk && reply_rdy);
-	reply_buf[15:8] = reply_data;
-	@(posedge fx2_clk && reply_rdy);
-	reply_buf[23:16] = reply_data;
-	@(posedge fx2_clk && reply_rdy);
-	reply_buf[31:24] = reply_data;
+	@(posedge fx2_clk && reply_rdy) reply_buf[7:0] = reply_data;
+	@(posedge fx2_clk && reply_rdy) reply_buf[15:8] = reply_data;
+	@(posedge fx2_clk && reply_rdy) reply_buf[23:16] = reply_data;
+	@(posedge fx2_clk && reply_rdy) reply_buf[31:24] = reply_data;
 	$display($time, "  Register %04x = %08x", addr, reply_buf);
 end
 endtask
 
-always @(posedge fx2_clk && sample_rdy)
-	$display($time, "  sample: %02x", sample_data);
+reg [47:0] sample_buf;
+initial begin
+#100 ; // Wait until things stabilize
+forever begin
+	@(posedge fx2_clk && sample_rdy);
+	@(posedge fx2_clk && sample_rdy) sample_buf[47:40] = sample_data;
+	@(posedge fx2_clk && sample_rdy) sample_buf[39:32] = sample_data;
+	@(posedge fx2_clk && sample_rdy) sample_buf[31:24] = sample_data;
+	@(posedge fx2_clk && sample_rdy) sample_buf[23:16] = sample_data;
+	@(posedge fx2_clk && sample_rdy) sample_buf[15:8] = sample_data;
+	@(posedge fx2_clk && sample_rdy) sample_buf[7:0] = sample_data;
+	$display($time, "  Sample %012x", sample_buf);
+end
+end
+
+//always @(posedge fx2_clk && sample_rdy) $display($time, "  sample: %02x", sample_data);
 	
 // This just prints the results in the ModelSim text window
 // You can leave this out if you want
