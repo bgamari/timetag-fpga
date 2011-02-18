351940c906c25a9368e8ad511769cb41601a9b73
Ben Gamari <bgamari.foss@gmail.com>
2011-02-17 21:38:56 -0500

    Fix off-by-one in reg_manager state machine

--- reg_manager.v
+++ reg_manager.v
@@ -36,71 +36,76 @@ case (state)
 	0:	if (cmd_in == 8'hAA)		// Read magic number
 			state <= 1;
 
-	1:	if (cmd_wr)			// Read message type
+	1:	if (cmd_wr)			// Read message type (read/write)
 		begin
 			wants_wr <= cmd_in[0];
 			state <= 2;
 		end
 
-	2:	if (cmd_wr)			// Read address, 1st byte
+	// Read address
+	2:	if (cmd_wr)			// 1st byte
 		begin
 			addr[7:0] <= cmd_in;
 			state <= 3;
 		end
-	3:	if (cmd_wr)			// Read address, 2nd byte
+	3:	if (cmd_wr)			// 2nd byte
 		begin
 			addr[15:8] <= cmd_in;
 			state <= 4;
 		end
 
-	5:	if (cmd_wr)			// Read data, 1st byte
+	// Read value
+	4:	if (cmd_wr)			// 1st byte
 		begin
 			data[7:0] <= cmd_in;
-			state <= 6;
+			state <= 5;
 		end
-	6:	if (cmd_wr)			// Read data, 2st byte
+	5:	if (cmd_wr)			// 2nd byte
 		begin
 			data[15:8] <= cmd_in;
-			state <= 7;
+			state <= 6;
 		end
-	7:	if (cmd_wr)			// Read data, 3rd byte
+	6:	if (cmd_wr)			// 3rd byte
 		begin
 			data[23:16] <= cmd_in;
-			state <= 8;
+			state <= 7;
 		end
-	8:	if (cmd_wr)			// Read data, 4th byte
+	7:	if (cmd_wr)			// 4th byte
 		begin
 			data[31:24] <= cmd_in;
-			state <= 9;
+			state <= 8;
 		end
-		
-	7:	state <= 8;			// Write new value to register (if needed)
 
-	8:	if (reply_ack)			// Reply with register value, 1st byte
-			state <= 9;
-	9:	if (reply_ack)			// Reply with register value, 1st byte
+	// Write new value to register (if needed)
+	8:	state <= 9;
+
+	// Write reply to host
+	9:	if (reply_ack)			// 1st byte
 			state <= 10;
-	10:	if (reply_ack)			// Reply with register value, 1st byte
+	10:	if (reply_ack)			// 2nd byte
 			state <= 11;
-	11:	if (reply_ack)			// Reply with register value, 1st byte
+	11:	if (reply_ack)			// 3rd byte
 			state <= 12;
+	12:	if (reply_ack)			// 4th byte
+			state <= 13;
 
-	12:	state <= 0;			// End reply packet
+	// End reply packet
+	13:	state <= 0;
 	
 	default:
 		state <= 0;
 endcase
 
-assign reg_addr = (state==7 || state==8 || state==9 || state==10) ? addr : 8'hXX;
-assign reg_data = (state==7) ? data : 8'hZZ;
-assign reg_wr = (state==7) && wants_wr;
+assign reg_addr = (state==8 || state==9 || state==10 || state==11 || state==12) ? addr : 8'hXX;
+assign reg_data = (state==8) ? data : 8'hZZ;
+assign reg_wr = (state==8) && wants_wr;
 
-assign reply_out = (state==8)  ? reg_data[7:0] : 
-		   (state==9)  ? reg_data[15:8] :
-		   (state==10) ? reg_data[23:16] :
-		   (state==11) ? reg_data[31:24] : 32'hZZZZZZZZ;
-assign reply_rdy = state==8 || state==9 || state==10 || state==11;
-assign reply_end = state==12;
+assign reply_out = (state==9)  ? reg_data[7:0] : 
+		   (state==10)	? reg_data[15:8] :
+		   (state==11) ? reg_data[23:16] :
+		   (state==12) ? reg_data[31:24] : 32'hZZZZZZZZ;
+assign reply_rdy = state==9 || state==10 || state==11 || state==12;
+assign reply_end = state==13;
 
 endmodule
 
